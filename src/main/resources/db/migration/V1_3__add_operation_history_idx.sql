0f7d37fd14a1ae944b0d80fded39602c051ab398
ggmaleva <ggmaleva@yandex.ru>
2024-10-16 16:19:34 +0300

    review (3)

--- src/main/resources/db/migration/V1_3__add_operation_history_idx.sql
+++ src/main/resources/db/migration/V1_3__add_operation_history_idx.sql
@@ -1,3 +1,4 @@
-CREATE INDEX operation_history_idx ON lim.operation_state_history USING btree (limit_name, state, created_at, operation_id);
-
 ALTER TABLE lim.operation_state_history ADD COLUMN limit_data_id bigint;
+
+CREATE INDEX operation_history_idx ON lim.operation_state_history USING btree (limit_data_id, state, created_at, operation_id);
+
